dcdad7fbc75131a4f535781d5197a48ea7efd044
Ivan Afante <afanteivanluigi@gmail.com>
2026-09-19 00:36:59 +0800

    Revert "Login Fixes"
    This reverts commit ee284b142fce3b5d3020e5eec6878ed135fe16fa.

--- Auto-Repair-Management-System/ARMSProtOne/LoginForm.Designer.cs
+++ Auto-Repair-Management-System/ARMSProtOne/LoginForm.Designer.cs
@@ -1,4 +1,4 @@
-namespace ARMSProtOne
+﻿namespace ARMSProtOne
 {
     partial class LoginForm
     {
@@ -12,236 +12,122 @@ namespace ARMSProtOne
 
         private void InitializeComponent()
         {
-            this.lblTitle1 = new System.Windows.Forms.Label();
-            this.lblTitle2 = new System.Windows.Forms.Label();
+            this.pnlCard = new System.Windows.Forms.Panel();
+            this.lblBusinessName = new System.Windows.Forms.Label();
             this.lblSubtitle = new System.Windows.Forms.Label();
-            this.lblUsernameField = new System.Windows.Forms.Label();
             this.picUserIcon = new System.Windows.Forms.PictureBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
-            this.lblPasswordField = new System.Windows.Forms.Label();
             this.picPasswordIcon = new System.Windows.Forms.PictureBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
-            this.picEyeIcon = new System.Windows.Forms.PictureBox();
-            this.chkRemember = new System.Windows.Forms.CheckBox();
-            this.btnLogin = new ARMSProtOne.GradientButton();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.lblDemoAccounts = new System.Windows.Forms.Label();
-            this.lblVersion = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picUserIcon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPasswordIcon)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picEyeIcon)).BeginInit();
+            this.pnlCard.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // lblTitle1
-            // 
-            this.lblTitle1.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitle1.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Bold);
-            this.lblTitle1.ForeColor = System.Drawing.Color.White;
-            this.lblTitle1.Location = new System.Drawing.Point(270, 186);
-            this.lblTitle1.Name = "lblTitle1";
-            this.lblTitle1.Size = new System.Drawing.Size(420, 24);
-            this.lblTitle1.TabIndex = 0;
-            this.lblTitle1.Text = "ARIAS CRUZ";
-            this.lblTitle1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblTitle2
-            // 
-            this.lblTitle2.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitle2.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Bold);
-            this.lblTitle2.ForeColor = System.Drawing.Color.White;
-            this.lblTitle2.Location = new System.Drawing.Point(270, 210);
-            this.lblTitle2.Name = "lblTitle2";
-            this.lblTitle2.Size = new System.Drawing.Size(420, 18);
-            this.lblTitle2.TabIndex = 1;
-            this.lblTitle2.Text = "C A R   A I R C O N   A U T O   E L E C T R I C A L";
-            this.lblTitle2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblSubtitle
-            // 
-            this.lblSubtitle.BackColor = System.Drawing.Color.Transparent;
-            this.lblSubtitle.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.lblSubtitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(168)))), ((int)(((byte)(180)))));
-            this.lblSubtitle.Location = new System.Drawing.Point(270, 234);
-            this.lblSubtitle.Name = "lblSubtitle";
-            this.lblSubtitle.Size = new System.Drawing.Size(420, 16);
-            this.lblSubtitle.TabIndex = 2;
-            this.lblSubtitle.Text = "Auto Repair Management System";
+
+            // ===== CARD =====
+            this.pnlCard.BackColor = System.Drawing.Color.FromArgb(0, 51, 153);
+            this.pnlCard.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlCard.Location = new System.Drawing.Point(200, 90);
+            this.pnlCard.Size = new System.Drawing.Size(500, 460);
+            this.pnlCard.Controls.Add(this.lblBusinessName);
+            this.pnlCard.Controls.Add(this.lblSubtitle);
+            this.pnlCard.Controls.Add(this.picUserIcon);
+            this.pnlCard.Controls.Add(this.txtUsername);
+            this.pnlCard.Controls.Add(this.picPasswordIcon);
+            this.pnlCard.Controls.Add(this.txtPassword);
+            this.pnlCard.Controls.Add(this.btnLogin);
+            this.pnlCard.Controls.Add(this.lblDemoAccounts);
+
+            // ===== BUSINESS NAME =====
+            this.lblBusinessName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.lblBusinessName.ForeColor = System.Drawing.Color.White;
+            this.lblBusinessName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblBusinessName.Location = new System.Drawing.Point(30, 35);
+            this.lblBusinessName.Size = new System.Drawing.Size(440, 60);
+            this.lblBusinessName.Text = "ARIAS CRUZ\r\nCAR AIRCON AUTO ELECTRICAL";
+
+            this.lblSubtitle.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lblSubtitle.ForeColor = System.Drawing.Color.Gainsboro;
             this.lblSubtitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblUsernameField
-            // 
-            this.lblUsernameField.BackColor = System.Drawing.Color.Transparent;
-            this.lblUsernameField.Font = new System.Drawing.Font("Segoe UI", 8.5F, System.Drawing.FontStyle.Bold);
-            this.lblUsernameField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(179)))), ((int)(((byte)(217)))));
-            this.lblUsernameField.Location = new System.Drawing.Point(310, 262);
-            this.lblUsernameField.Name = "lblUsernameField";
-            this.lblUsernameField.Size = new System.Drawing.Size(340, 14);
-            this.lblUsernameField.TabIndex = 3;
-            this.lblUsernameField.Text = "Username";
-            // 
-            // picUserIcon
-            // 
-            this.picUserIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
-            this.picUserIcon.Location = new System.Drawing.Point(320, 288);
-            this.picUserIcon.Name = "picUserIcon";
-            this.picUserIcon.Size = new System.Drawing.Size(24, 24);
+            this.lblSubtitle.Location = new System.Drawing.Point(30, 100);
+            this.lblSubtitle.Size = new System.Drawing.Size(440, 24);
+            this.lblSubtitle.Text = "Auto Repair Management System";
+
+            // ===== USERNAME =====
+            this.picUserIcon.BackColor = System.Drawing.Color.FromArgb(0, 41, 128);
+            this.picUserIcon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picUserIcon.Location = new System.Drawing.Point(60, 165);
+            this.picUserIcon.Size = new System.Drawing.Size(36, 36);
             this.picUserIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.picUserIcon.TabIndex = 4;
-            this.picUserIcon.TabStop = false;
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
-            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+
             this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 10.5F);
-            this.txtUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(246)))), ((int)(((byte)(249)))));
-            this.txtUsername.Location = new System.Drawing.Point(354, 289);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(256, 19);
-            this.txtUsername.TabIndex = 0;
-            // 
-            // lblPasswordField
-            // 
-            this.lblPasswordField.BackColor = System.Drawing.Color.Transparent;
-            this.lblPasswordField.Font = new System.Drawing.Font("Segoe UI", 8.5F, System.Drawing.FontStyle.Bold);
-            this.lblPasswordField.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(179)))), ((int)(((byte)(217)))));
-            this.lblPasswordField.Location = new System.Drawing.Point(310, 334);
-            this.lblPasswordField.Name = "lblPasswordField";
-            this.lblPasswordField.Size = new System.Drawing.Size(340, 14);
-            this.lblPasswordField.TabIndex = 5;
-            this.lblPasswordField.Text = "Password";
-            // 
-            // picPasswordIcon
-            // 
-            this.picPasswordIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
-            this.picPasswordIcon.Location = new System.Drawing.Point(320, 360);
-            this.picPasswordIcon.Name = "picPasswordIcon";
-            this.picPasswordIcon.Size = new System.Drawing.Size(24, 24);
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.ForeColor = System.Drawing.Color.FromArgb(33, 37, 41);
+            this.txtUsername.Location = new System.Drawing.Point(104, 165);
+            this.txtUsername.Size = new System.Drawing.Size(336, 28);
+
+            // ===== PASSWORD =====
+            this.picPasswordIcon.BackColor = System.Drawing.Color.FromArgb(0, 41, 128);
+            this.picPasswordIcon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPasswordIcon.Location = new System.Drawing.Point(60, 215);
+            this.picPasswordIcon.Size = new System.Drawing.Size(36, 36);
             this.picPasswordIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.picPasswordIcon.TabIndex = 6;
-            this.picPasswordIcon.TabStop = false;
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
-            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+
             this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 10.5F);
-            this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(246)))), ((int)(((byte)(249)))));
-            this.txtPassword.Location = new System.Drawing.Point(354, 361);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(222, 19);
-            this.txtPassword.TabIndex = 1;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.ForeColor = System.Drawing.Color.FromArgb(33, 37, 41);
+            this.txtPassword.Location = new System.Drawing.Point(104, 215);
+            this.txtPassword.Size = new System.Drawing.Size(336, 28);
             this.txtPassword.UseSystemPasswordChar = true;
-            // 
-            // picEyeIcon
-            // 
-            this.picEyeIcon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
-            this.picEyeIcon.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.picEyeIcon.Location = new System.Drawing.Point(616, 360);
-            this.picEyeIcon.Name = "picEyeIcon";
-            this.picEyeIcon.Size = new System.Drawing.Size(24, 24);
-            this.picEyeIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.picEyeIcon.TabIndex = 7;
-            this.picEyeIcon.TabStop = false;
-            this.picEyeIcon.Click += new System.EventHandler(this.picEyeIcon_Click);
-            // 
-            // chkRemember
-            // 
-            this.chkRemember.BackColor = System.Drawing.Color.Transparent;
-            this.chkRemember.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.chkRemember.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.chkRemember.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(196)))), ((int)(((byte)(206)))));
-            this.chkRemember.Location = new System.Drawing.Point(310, 406);
-            this.chkRemember.Name = "chkRemember";
-            this.chkRemember.Size = new System.Drawing.Size(200, 20);
-            this.chkRemember.TabIndex = 2;
-            this.chkRemember.Text = "Remember this device";
-            this.chkRemember.UseVisualStyleBackColor = false;
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
-            this.btnLogin.CornerRadius = 10;
+
+            // ===== LOGIN BUTTON =====
+            this.btnLogin.BackColor = System.Drawing.Color.White;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
-            this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(310, 440);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(340, 46);
-            this.btnLogin.TabIndex = 3;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10.5F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.Color.FromArgb(0, 51, 153);
+            this.btnLogin.Location = new System.Drawing.Point(60, 275);
+            this.btnLogin.Size = new System.Drawing.Size(380, 42);
             this.btnLogin.Text = "LOGIN";
             this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-            // 
-            // lblDemoAccounts
-            // 
-            this.lblDemoAccounts.BackColor = System.Drawing.Color.Transparent;
-            this.lblDemoAccounts.Font = new System.Drawing.Font("Segoe UI", 7.5F, System.Drawing.FontStyle.Italic);
-            this.lblDemoAccounts.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(151)))), ((int)(((byte)(162)))));
-            this.lblDemoAccounts.Location = new System.Drawing.Point(310, 514);
-            this.lblDemoAccounts.Name = "lblDemoAccounts";
-            this.lblDemoAccounts.Size = new System.Drawing.Size(340, 14);
-            this.lblDemoAccounts.TabIndex = 8;
-            this.lblDemoAccounts.Text = "Demo - admin/1234  |  staff/1234";
+
+            // ===== DEMO ACCOUNTS =====
+            this.lblDemoAccounts.Font = new System.Drawing.Font("Segoe UI", 8.5F, System.Drawing.FontStyle.Italic);
+            this.lblDemoAccounts.ForeColor = System.Drawing.Color.Gainsboro;
             this.lblDemoAccounts.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblVersion
-            // 
-            this.lblVersion.BackColor = System.Drawing.Color.Transparent;
-            this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 7F);
-            this.lblVersion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(110)))), ((int)(((byte)(120)))));
-            this.lblVersion.Location = new System.Drawing.Point(310, 532);
-            this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(340, 14);
-            this.lblVersion.TabIndex = 9;
-            this.lblVersion.Text = "Secure connection - v1.4.4";
-            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // LoginForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(24)))), ((int)(((byte)(28)))));
-            this.ClientSize = new System.Drawing.Size(960, 660);
-            this.Controls.Add(this.lblTitle1);
-            this.Controls.Add(this.lblTitle2);
-            this.Controls.Add(this.lblSubtitle);
-            this.Controls.Add(this.lblUsernameField);
-            this.Controls.Add(this.picUserIcon);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.lblPasswordField);
-            this.Controls.Add(this.picPasswordIcon);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.picEyeIcon);
-            this.Controls.Add(this.chkRemember);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.lblDemoAccounts);
-            this.Controls.Add(this.lblVersion);
+            this.lblDemoAccounts.Location = new System.Drawing.Point(30, 345);
+            this.lblDemoAccounts.Size = new System.Drawing.Size(440, 60);
+            this.lblDemoAccounts.Text = "Demo Accounts (for testing)\r\nAdmin — user: admin  pass: 1234\r\nStaff — user: staff  pass: 1234";
+
+            // ===== ROOT =====
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(52, 58, 64);
+            this.ClientSize = new System.Drawing.Size(900, 640);
+            this.Controls.Add(this.pnlCard);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login - Arias Cruz Car Aircon Auto Electrical";
+            this.Resize += new System.EventHandler(this.LoginForm_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.picUserIcon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPasswordIcon)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picEyeIcon)).EndInit();
+            this.pnlCard.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
-        private System.Windows.Forms.Label lblTitle1;
-        private System.Windows.Forms.Label lblTitle2;
+        private System.Windows.Forms.Panel pnlCard;
+        private System.Windows.Forms.Label lblBusinessName;
         private System.Windows.Forms.Label lblSubtitle;
-        private System.Windows.Forms.Label lblUsernameField;
         private System.Windows.Forms.PictureBox picUserIcon;
         private System.Windows.Forms.TextBox txtUsername;
-        private System.Windows.Forms.Label lblPasswordField;
         private System.Windows.Forms.PictureBox picPasswordIcon;
         private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.PictureBox picEyeIcon;
-        private System.Windows.Forms.CheckBox chkRemember;
-        private GradientButton btnLogin;
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lblDemoAccounts;
-        private System.Windows.Forms.Label lblVersion;
     }
 }
--- Auto-Repair-Management-System/ARMSProtOne/LoginForm.cs
+++ Auto-Repair-Management-System/ARMSProtOne/LoginForm.cs
@@ -1,6 +1,11 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ARMSProtOne
@@ -14,105 +19,22 @@ namespace ARMSProtOne
         private const string DemoStaffUser = "staff";
         private const string DemoStaffPass = "1234";
 
-        private Bitmap userGlyph, lockGlyph, eyeGlyph;
-
-        private Rectangle CardRect => new Rectangle((ClientSize.Width - 420) / 2, (ClientSize.Height - 480) / 2, 420, 480);
-        private Rectangle PillUserRect => new Rectangle(CardRect.X + 40, CardRect.Y + 188, 340, 44);
-        private Rectangle PillPassRect => new Rectangle(CardRect.X + 40, CardRect.Y + 260, 340, 44);
-
         public LoginForm()
         {
             InitializeComponent();
-            this.DoubleBuffered = true;
-            this.AcceptButton = btnLogin;
-            LoadGlyphs(); //To Remove, replace with image icons
+            CenterCard();
         }
 
-        private void LoadGlyphs() //To Remove, replace with image icons
+        private void LoginForm_Resize(object sender, EventArgs e)
         {
-            userGlyph = Theme.CreateGlyph(Theme.GlyphType.User, Theme.FieldLabel);
-            lockGlyph = Theme.CreateGlyph(Theme.GlyphType.Lock, Theme.FieldLabel);
-            eyeGlyph = Theme.CreateGlyph(Theme.GlyphType.Eye, Theme.FieldLabel);
-            picUserIcon.Image = userGlyph;
-            picPasswordIcon.Image = lockGlyph;
-            picEyeIcon.Image = eyeGlyph;
+            CenterCard();
         }
 
-        protected override void OnPaint(PaintEventArgs e)
+        private void CenterCard()
         {
-            base.OnPaint(e);
-            var g = e.Graphics;
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-
-            using (var bgBrush = new LinearGradientBrush(ClientRectangle, Theme.BgTop, Theme.BgBottom, 135f))
-                g.FillRectangle(bgBrush, ClientRectangle);
-
-            DrawGlow(g, new Point(0, 0), Theme.GlowTL);
-            DrawGlow(g, new Point(ClientSize.Width, ClientSize.Height), Theme.GlowBR);
-
-            var card = CardRect;
-
-            for (int i = 3; i >= 1; i--)
-            {
-                var glowRect = Rectangle.Inflate(card, i * 2, i * 2);
-                int alpha = i == 1 ? 28 : (i == 2 ? 16 : 8);
-                using (var path = Theme.RoundedRect(glowRect, 16 + i * 2))
-                using (var pen = new Pen(Color.FromArgb(alpha, Theme.CardGlow), 1.5f))
-                    g.DrawPath(pen, path);
-            }
-
-            using (var cardPath = Theme.RoundedRect(card, 16))
-            using (var cardBrush = new LinearGradientBrush(card, Theme.CardTop, Theme.CardBottom, 90f))
-            {
-                g.FillPath(cardBrush, cardPath);
-                using (var borderPen = new Pen(Theme.CardBorder, 1f))
-                    g.DrawPath(borderPen, cardPath);
-            }
-
-            var logoRect = new Rectangle(card.X + 182, card.Y + 28, 56, 56);
-            using (var logoPath = Theme.RoundedRect(logoRect, 14))
-            using (var logoBrush = new LinearGradientBrush(logoRect, Theme.LogoStart, Theme.LogoEnd, 45f))
-            {
-                g.FillPath(logoBrush, logoPath);
-                TextRenderer.DrawText(g, "AC", new Font("Segoe UI", 18F, FontStyle.Bold), logoRect, Color.White,
-                    TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
-            }
-
-            DrawPill(g, PillUserRect);
-            DrawPill(g, PillPassRect);
-
-            using (var dividerPen = new Pen(Theme.DividerColor, 1f))
-                g.DrawLine(dividerPen, card.X + 40, card.Y + 414, card.X + 380, card.Y + 414);
-        }
-
-        private void DrawGlow(Graphics g, Point center, Color color)
-        {
-            using (var path = new GraphicsPath())
-            {
-                path.AddEllipse(center.X - 220, center.Y - 220, 440, 440);
-                using (var brush = new PathGradientBrush(path))
-                {
-                    brush.CenterColor = Color.FromArgb(60, color);
-                    brush.SurroundColors = new[] { Color.Transparent };
-                    g.FillPath(brush, path);
-                }
-            }
-        }
-
-        private void DrawPill(Graphics g, Rectangle rect)
-        {
-            using (var path = Theme.RoundedRect(rect, 10))
-            using (var brush = new SolidBrush(Theme.PillFill))
-            using (var pen = new Pen(Theme.PillBorder, 1f))
-            {
-                g.FillPath(brush, path);
-                g.DrawPath(pen, path);
-            }
-        }
-
-        private void picEyeIcon_Click(object sender, EventArgs e)
-        {
-            txtPassword.UseSystemPasswordChar = !txtPassword.UseSystemPasswordChar;
+            pnlCard.Location = new System.Drawing.Point(
+                (this.ClientSize.Width - pnlCard.Width) / 2,
+                (this.ClientSize.Height - pnlCard.Height) / 2);
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
@@ -149,14 +71,6 @@ namespace ARMSProtOne
             var mainForm = new BaseForm();
             mainForm.FormClosed += (s, ev) => this.Close();
             mainForm.Show();
-        }
-
-        protected override void OnFormClosed(FormClosedEventArgs e)
-        {
-            userGlyph?.Dispose();
-            lockGlyph?.Dispose();
-            eyeGlyph?.Dispose();
-            base.OnFormClosed(e);
         }
     }
 }
